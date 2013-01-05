4d375a9c58d870385453f25bdad272aa232f5189
Themakew <ruy_fusion@hotmail.com>
2013-01-05 16:30:03 -0200

    Update Codigo/funcoes/cadastra_chamado.php

--- Codigo/funcoes/cadastra_chamado.php
+++ Codigo/funcoes/cadastra_chamado.php
@@ -1,4 +1,15 @@
 <?php
+include "conexao.php";
+include '../classes_de_negocio/Comentario.class.php';
 
+    
+    $comentario= new Comentario($_POST[''],$_POST['codigo'],$_POST['descriçao'],$_POST['cod_comentario']);
+    $sql = mysql_query("INSERT INTO solicitante (codigo, descriçao, cod_comentario) values ('$->nome', '$comentario->codigo','$comentario->descriçao', '')");
+    
+    
+    //$sql = mysql_query("INSERT INTO usuario (login, senha, codigo_solicitante) values ('$user->nome_usuario', '$user->senha', '')");
+    echo "<script type=\"text/javascript\">
+            alert(\"Cadastro efetuado\");
+            </script>";
 
 ?>
