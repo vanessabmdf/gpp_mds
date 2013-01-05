6be5ccd09271c6404dcc1715fbe6ce90e79e7470
Vanessa Barbosa <vanessa_naruto@hotmail.com>
2013-01-05 16:18:38 -0200

    nhaaa

--- Codigo/funcoes/cadastra.php
+++ Codigo/funcoes/cadastra.php
@@ -6,14 +6,22 @@ include '../classes_de_negocio/Solicitante.class.php';
 if($_POST['senha']!=$_POST['confirmasenha']){
     header("Location: ../interface/novocadastro.php");
 }else{
-    
+        
+   
     $solicitante= new Solicitante($_POST['nome'],$_POST['email'],$_POST['matricula'],$_POST['data_nascimento']);
-    //$user = new Usuario($_POST['login'], $_POST['senha'], NULL );
-    echo $solicitante->getNome();
-    $sql = mysql_query("INSERT INTO solicitante (nome, email,matricula) values ('$solicitante->nome', '$solicitante->email','$solicitante->matricula)");
+    $user = new Usuario($_POST['login'], $_POST['senha'], NULL );
     
+    $sql = mysql_query("INSERT INTO `helpdesk`.`solicitante` (`nome`, `email`, `matricula`, `codigo`, `data_nascimento`) VALUES ('$solicitante->nome', '$solicitante->email', '$solicitante->matricula', '', '$solicitante->data_nascimento')");
     
-    //$sql = mysql_query("INSERT INTO usuario (login, senha, codigo_solicitante) values ('$user->nome_usuario', '$user->senha', '')");
+    $sql = mysql_query("SELECT * FROM solicitante WHERE matricula = '$solicitante->matricula'");
+
+    if(mysql_num_rows($sql)){
+	while($ln = mysql_fetch_array($sql)){
+        
+        $cod=$ln['codigo'];
+        $sql2 = mysql_query("INSERT INTO `helpdesk`.`usuario` (`login`, `senha`, `codigo_perfil`, `codigo_solicitante`, `codigo`) VALUES ('$user->nome_usuario', '$user->senha', '', $cod, '')");
+        }
+    }
     echo "<script type=\"text/javascript\">
             alert(\"Cadastro efetuado\");
             </script>";
