a0054d246f2687d5487079bb4b03323238229ea2
Vanessa Barbosa <vanessa_naruto@hotmail.com>
2013-01-05 16:27:17 -0200

    atualização...

--- Codigo/funcoes/sair.php
+++ Codigo/funcoes/sair.php
@@ -1,11 +1,8 @@
 <?php
 	session_start();
 	if(isset($_SESSION['login']) and isset($_SESSION['senha'])){
-            echo 'page';
             unset($_SESSION['login']);
             unset($_SESSION['senha']);
-            
-            echo $_SESSION['login']; 
-           // header("Location: ../interface/home.php");
+           header("Location: ../interface/home.php");
 	}
 ?>
--- Codigo/interface/chamado.php
+++ Codigo/interface/chamado.php
@@ -3,26 +3,41 @@
 <?php
     include "topo.php"
 ?>
-<html>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</html>
+
 <div id="conteudo">
 <div id="cont">
 
-<h2>Página de chamado</h2>
+<h2>Cadastrar Chamado</h2>
 
-<?php
-	session_start();
-	if(!isset($_SESSION['login']) and !isset($_SESSION['senha'])){
-        echo("Faça o login para acessar o conteúdo da página");
-	
-	exit;
-	}else{
-            echo("logado");
-        }
-?>
+<hr> 
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<body>
+
+<form method="post" action="../funcoes/cadastra.php">
+<table width="280" border="0">
+  <tr>
+    <td width="133">Solicitante(Codigo):</td>
+    <td width="137"><input type="text" name="codigo_solicitante" /></td>
+  </tr>
+  <tr>
+    <td>Data Inicial:</td>
+    <td><input type="text" name="data_inicial"/></td>
+  </tr>
+  <tr>
+    <td>Descricao:</td>
+    <td><input type="text" name="descricao"/></td>
+  </tr>
+
+   <tr>
+        <td align="center">&nbsp;</td>
+        <td> <input type="submit" value="Cadastrar" /></td>
+        </tr>
+</table>
+</form>
+</body>
+</html>
 </div><!-- fim div cont -->
-
 </div> <!-- fim div conteudo -->
 <?php
     include "rodape.php"
