5443ea43a1dc2e8dfd8f1a731e1ee1cf40c020f6
Vanessa Barbosa <vanessa_naruto@hotmail.com>
2013-01-05 23:34:16 -0200

    chamado

--- Codigo/funcoes/cadastra_chamado.php
+++ Codigo/funcoes/cadastra_chamado.php
@@ -7,7 +7,9 @@ include '../classes_de_negocio/Chamado.class.php';
 
     $login= $_SESSION['login'];
     $sql = mysql_query("SELECT * FROM usuario WHERE login = '$login'");
+    $ln = mysql_fetch_array($sql);
     
+    $cod_solicitante = $ln['cod_solicitante'];
     //chamado= new Chamado();
     
 ?>
