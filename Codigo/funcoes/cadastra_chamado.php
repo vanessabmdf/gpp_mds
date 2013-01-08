6978c15bf6d0b01a6d5089aeb968815a5c4dd70c
Vanessa Barbosa <vanessa_naruto@hotmail.com>
2013-01-08 12:05:38 -0200

    ---

--- Codigo/funcoes/cadastra_chamado.php
+++ Codigo/funcoes/cadastra_chamado.php
@@ -7,10 +7,11 @@ include '../classes_de_negocio/Chamado.class.php';
     $sql = mysql_query("SELECT * FROM usuario WHERE login = '$login'");
     $ln = mysql_fetch_array($sql);
     $cod_solicitante = $ln['codigo_solicitante'];
-    
-    $chamado = new Chamado(NULL, NULL, $_POST['descricao'], NULL, NULL, NULL, $cod_solicitante);
-    $bd = mysql_query("INSERT INTO `helpdesk`.`chamado` (`codigo`, `descricao`, `data_inicial`, `data_final`, `codigo_status`, `codigo_servico`, `codigo_tecnico`, `codigo_solicitante`) VALUES ('', '$chamado->descricao', '', '', '', '', '', '$chamado->cod_solicitante')");
-    echo "<meta http-equiv='refresh' content='0; URL=../interface/chamado.php'>
+    $dataI = date("Y").'-'.date("m").'-'.date("d");
+    $dataF = date("Y").'-'.date("m").'-'.(date("d")+7);
+    $chamado = new Chamado(NULL, NULL, $_POST['descricao'], $dataI, $dataF, NULL, $cod_solicitante);
+    $bd = mysql_query("INSERT INTO `helpdesk`.`chamado` (`codigo`, `descricao`, `data_inicial`, `data_final`, `codigo_status`, `codigo_servico`, `codigo_tecnico`, `codigo_solicitante`) VALUES ('', '$chamado->descricao', '$chamado->data_inicial', '$chamado->data_final', '', '', '', '$chamado->cod_solicitante')");
+    echo "<meta http-equiv='refresh' content='0; URL=../interface/index.php'>
 		<script type=\"text/javascript\">
 		alert(\"Chamado cadastrado\");
 		</script>";
