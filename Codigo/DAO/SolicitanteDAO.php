60211a244fce4e1b3e16b98d63eded199c249598
Luiz Matos <luizff.matos@gmail.com>
2013-01-09 18:48:57 -0200

    Adição da função de alterarSolicitante

--- Codigo/DAO/SolicitanteDAO.php
+++ Codigo/DAO/SolicitanteDAO.php
@@ -29,11 +29,35 @@ class SolicitanteDAO {
             $stm->bindParam(":matricula", $solicitante->getMatricula());
             $stm->bindParam(":data_nascimento", $solicitante->getData());
             $exec = $stm->execute();
-            //$this->con = null;
+            
         } catch (PDOException $erro) {
             echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
         }
     }
+    
+        public function alteraSolicitante(Solicitante $solicitante, Usuario $usuario, $codigo) {
+        try {
+            /* Sem passagem por referência
+              $nome = $solicitante->getNome();
+              $email = $solicitante->getEmail();
+              $matricula = $solicitante->getMatricula();
+              $data = $solicitante->getData();
+             */
+            $query = "UPDATE SET nome=:nome, email=:email, matricula:matricula, data_nascimento:data_nascimento
+                      WHERE codigo = ".$codigo.""; 
+                      
+            $stm = $this->con->prepare($query);
+            $stm->bindParam(":nome", $solicitante->getNome());
+            $stm->bindParam(":email", $solicitante->getEmail());
+            $stm->bindParam(":matricula", $solicitante->getMatricula());
+            $stm->bindParam(":data_nascimento", $solicitante->getData());       
+            $stm->execute();
+            $this->con->commit();
+        } catch (PDOException $erro) {
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
+    }
+
 
     public function obterSolicitante() {
         try {
@@ -43,7 +67,7 @@ class SolicitanteDAO {
             echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
         }
     }
-
+    //Função de fechar a conexão aberta no DAO
     public function fechaConexão() {
         try {
             $this->con = null;
