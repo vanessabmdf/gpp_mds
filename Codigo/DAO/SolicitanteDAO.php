efdf12aea080a4bc7f5c2e533373521761204b4f
Luiz Matos <luizff.matos@gmail.com>
2013-01-09 18:18:36 -0200

    Adição da função de obterSolicitante e fechamento da conexao aberta no DAO

--- Codigo/DAO/SolicitanteDAO.php
+++ Codigo/DAO/SolicitanteDAO.php
@@ -5,32 +5,51 @@ require_once "../model/Solicitante.class.php";
 
 class SolicitanteDAO {
 
-    private $con; 
-    public function SolicitanteDAO(){
+    public $con;
+
+    public function SolicitanteDAO() {
         $this->con = new Conection();
         //Função para aparecer os erros
         $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function insereSolicitante(Solicitante $solicitante, Usuario $usuario) {
-        try{
-            /*Sem passagem por referência
-            $nome = $solicitante->getNome();
-            $email = $solicitante->getEmail();
-            $matricula = $solicitante->getMatricula();
-            $data = $solicitante->getData();
-            */
-            $stm = $this->con->prepare("INSERT INTO solicitante (nome, email, matricula, data_nascimento) 
-                                  VALUES (:nome, :email, :matricula, :data_nascimento)");
-            $stm->bindParam(":nome",$solicitante->getNome());
-            $stm->bindParam(":email",$solicitante->getEmail());
-            $stm->bindParam(":matricula",$solicitante->getMatricula());
-            $stm->bindParam(":data_nascimento",$solicitante->getData());
+        try {
+            /* Sem passagem por referência
+              $nome = $solicitante->getNome();
+              $email = $solicitante->getEmail();
+              $matricula = $solicitante->getMatricula();
+              $data = $solicitante->getData();
+             */
+            $query = "INSERT INTO solicitante (nome, email, matricula, data_nascimento) 
+                      VALUES (:nome, :email, :matricula, :data_nascimento)";
+            $stm = $this->con->prepare($query);
+            $stm->bindParam(":nome", $solicitante->getNome());
+            $stm->bindParam(":email", $solicitante->getEmail());
+            $stm->bindParam(":matricula", $solicitante->getMatricula());
+            $stm->bindParam(":data_nascimento", $solicitante->getData());
             $exec = $stm->execute();
-            $this->con->null;
-        }catch (Exception $erro){
-            echo "Ocorreu um erro na operação, informe o erro ao CPD: ". $erro->getMessage();           
-        }        
+            //$this->con = null;
+        } catch (PDOException $erro) {
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
+    }
+
+    public function obterSolicitante() {
+        try {
+            $stm = $this->con->query("SELECT * FROM solicitante");
+            return $stm;
+        } catch (PDOException $erro) {
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
+    }
+
+    public function fechaConexão() {
+        try {
+            $this->con = null;
+        } catch (PDOException $erro) {
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
     }
 
 }
