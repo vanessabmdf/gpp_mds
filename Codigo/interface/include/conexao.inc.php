aab47831dcce8b7763cc60b78b30ca52d7f1a152
Luiz Matos <luizff.matos@gmail.com>
2013-01-10 02:13:19 -0200

    Mudanças nos arquivos

--- Codigo/interface/include/conexao.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$conn = mysql_connect("127.0.0.1","root","root") or die ("Nao foi possivel conectar");
-	$db = mysql_select_db("gti",$conn) or die ("Nao foi possivel localizar o banco de dados");
-?>
