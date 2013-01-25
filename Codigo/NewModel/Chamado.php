21635e527ac80d725e2d09ad69166a28ec4f9a25
Filipe Borges <filipe_bkc@yahoo.com.br>
2013-01-25 15:11:34 -0200

    Alteracoes do diagrama de classes e de Usuario.php e Chamado.php

--- Codigo/NewModel/Chamado.php
+++ Codigo/NewModel/Chamado.php
@@ -6,7 +6,6 @@ class Chamado
     private $data_inicial;
     private $data_final;
     private $descricao;
-    private $comentarioChamado;
     private $solicitante;
     private $tecnico;
     private $status;
@@ -54,14 +53,6 @@ class Chamado
 
     public function setDescricao($descricao) {
         $this->descricao = $descricao;
-    }
-
-    public function getComentarioChamado() {
-        return $this->comentarioChamado;
-    }
-
-    public function setComentarioChamado($comentarioChamado) {
-        $this->comentarioChamado = $comentarioChamado;
     }
 
     public function getSolicitante() {
--- Codigo/NewModel/Usuario.php
+++ Codigo/NewModel/Usuario.php
@@ -3,27 +3,43 @@
 class Usuario
 {
     private $codigo;
-    private $tipo;
     private $login;
     private $senha;
     private $nome;
     private $email;
     private $matricula;
-    private $data_nascimento;
+    private $local_patrimonio;
+    private $numero_parimonio;
     
-    //O código tem que ser gerado pelo sistema, e não pode ser informado pelo usuário no ato do cadastro.
+       //O código tem que ser gerado pelo sistema, e não pode ser informado pelo usuário no ato do cadastro.
     /*Agente pode fazer um cod. sequencial de acordo com os usuarios que forem sendo cadastrados
      * por exemplo: 01, 02, 03, 04, 05, ...
-     */
-    function __construct($codigo, $tipo, $login, $senha, $nome, $email, $matricula, $data_nascimento) {
+     */    
+    function __construct($codigo, $login, $senha, $nome, $email, $matricula, $local_patrimonio, $numero_parimonio) {
         $this->codigo = $codigo;
-        $this->tipo = $tipo;
         $this->login = $login;
         $this->senha = $senha;
         $this->nome = $nome;
         $this->email = $email;
         $this->matricula = $matricula;
-        $this->data_nascimento = $data_nascimento;
+        $this->local_patrimonio = $local_patrimonio;
+        $this->numero_parimonio = $numero_parimonio;
+    }
+
+    public function getLocal_patrimonio() {
+        return $this->local_patrimonio;
+    }
+
+    public function setLocal_patrimonio($local_patrimonio) {
+        $this->local_patrimonio = $local_patrimonio;
+    }
+
+    public function getNumero_parimonio() {
+        return $this->numero_parimonio;
+    }
+
+    public function setNumero_parimonio($numero_parimonio) {
+        $this->numero_parimonio = $numero_parimonio;
     }
     
     public function getCodigo() {
@@ -32,14 +48,6 @@ class Usuario
 
     public function setCodigo($codigo) {
         $this->codigo = $codigo;
-    }
-
-    public function getTipo() {
-        return $this->tipo;
-    }
-
-    public function setTipo($tipo) {
-        $this->tipo = $tipo;
     }
 
     public function getLogin() {
@@ -80,14 +88,6 @@ class Usuario
 
     public function setMatricula($matricula) {
         $this->matricula = $matricula;
-    }
-
-    public function getData_nascimento() {
-        return $this->data_nascimento;
-    }
-
-    public function setData_nascimento($data_nascimento) {
-        $this->data_nascimento = $data_nascimento;
-    }    
+    }   
 }
 ?>
