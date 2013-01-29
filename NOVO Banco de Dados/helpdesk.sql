ef0079fa27278979ef9b42f1393a4c97e013b747
Vanessa <vanessabmdf@gmail.com>
2013-01-29 20:23:27 -0200

    BD de acordo com o último diagrama
    Por favor deêm feedbacks o mais rápido possível, pra concertar os erros

--- NOVO Banco de Dados/helpdesk.sql
+++ NOVO Banco de Dados/helpdesk.sql
@@ -4,8 +4,8 @@
 --
 -- Servidor: 127.0.0.1
 -- Tempo de Geração: 
--- Versão do Servidor: 5.5.27-log
--- Versão do PHP: 5.4.6
+-- Versão do Servidor: 5.5.27
+-- Versão do PHP: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -35,8 +35,21 @@ CREATE TABLE IF NOT EXISTS `solicitante` (
   `nome_usuario` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `senha_usuario` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
 
+--
+-- Extraindo dados da tabela `solicitante`
+--
+
+INSERT INTO `solicitante` (`codigo`, `nome`, `email`, `matricula`, `data_nascimento`, `nome_usuario`, `senha_usuario`) VALUES
+(2, 'lui111zff.mat33os@gmail.com', 'luizff.matos@gmail.com', 45355433, '1212-12-12', '123456', '1212121212'),
+(5, '1111111111', '1111111@11.com', 2147483647, '0123-01-01', '134434', '2311231231'),
+(6, 'Luiz11', '11111@lskjd.c9ojn', 23230923, '0000-00-00', '029302390', '1092303923'),
+(7, '11111111111', '11111@sddsd.com', 2938923, '0000-00-00', '20932093', '092309230932'),
+(8, '11111', '111111@sdds.com', 23232323, '0000-00-00', '232323', '323232323'),
+(9, '121212', '121221@2323.com', 82738237, '0000-00-00', '82738237832', '872873897398131231'),
+(10, '111111111', '111111111@sdsd.com', 2930293, '0120-12-21', '2300023032', '0320230203'),
+(11, 'teste123', 'teste@mail.com', 100131192, '1993-10-12', 'vanessa', 'hunhun');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
