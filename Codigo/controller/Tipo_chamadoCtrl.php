8fd527a1115ad21ba5f330c75ba8c323ee9be3f9
Ruyther <ruy_fusion@hotmail.com>
2013-02-03 12:00:23 -0200

    Tipo_ChamadoCrtl.php

---
+++ Codigo/controller/Tipo_chamadoCtrl.php
@@ -0,0 +1,57 @@
+<?php
+
+    require_once "/../DAO/Tipo_ChamadoDAO.php";
+    require_once '/../Model/Tipo_Chamado.php';
+    
+    class Tipo_chamadoCtrl{
+        
+        public function insChamado($codigo,$descricao){
+            try {
+                $tipo_chamado = new Tipo_Chamado($codigo, $descricao);
+                
+                $DAO = new Tipo_ChamadoDAO();
+                $resultado = $DAO->inserirTipo_Chamado($tipo_chamado);
+                $DAO->fechaConexão();
+                return $resultado;                 
+        } catch (Exception $erro){
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
+            
+    }
+    
+        Public function alterarTipo_Chamado($codigo, $descricao, $cod_busca_chamado){
+            try{
+                    $tipo_chamado = new Tipo_Chamado($codigo, $descricao);
+                    
+                    $DAO = new Tipo_ChamadoDAO();
+                    $resultado = $DAO->alterarTipo_Chamado($tipo_chamado, $cod_busca_chamado);
+                    $DAO->fechaConexão();
+                    return $resultado; 
+                } catch (Exception $erro){
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
+        }
+        
+        Public function obterTipo_Chamado_Especifico($codigoTipo_Chamado){
+            
+                    $tipo_chamado = new Tipo_chamado($codigoTipo_Chamado);
+                    $DAO = new Tipo_ChamadoDAO();
+                    $DAO->fechaConexão();
+                    return $tipo_chamado;
+        }
+        
+        Public function deletarTipo_Chamado($codigoTipo_Chamado){
+            try{
+                
+                    $tipo_chamado = new Tipo_Chamado($codigoTipo_Chamado);
+                    $DAO = new Tipo_Chamado($tipo_chamado);
+                    $DAO->fechaConexão();
+                    return tipo_chamado;
+        }catch (Exception $erro){
+            echo "Ocorreu um erro na operação, informe o erro ao CPD: " . $erro->getMessage();
+        }
+        }
+        
+}
+       
+?>
